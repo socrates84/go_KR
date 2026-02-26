c06576f9c6a6546419b15c685331f4187f78de61
socrates <startup.developer84@gmail.com>
2026-02-25 22:47:02 -0500

    revised ex1.10

--- K_R/ch1/ex1.10/ex1.10.go
+++ K_R/ch1/ex1.10/ex1.10.go
@@ -6,7 +6,7 @@
  * This makes tabs and backslashes visible in an unambiguous
  * way
  *
- * Author: Alain
+ * Author: socrates
  *
  **********************************************************/
  package main
