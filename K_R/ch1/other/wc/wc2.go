606cdde59caf1b7e1a6103dcd405c7d8d78eb1ec
socrates <startup.developer84@gmail.com>
2026-02-28 22:14:15 -0500

    added wc2.go

---
+++ K_R/ch1/other/wc/wc2.go
@@ -0,0 +1,62 @@
+/**********************************************************
+ * File: wc2.go
+ *
+ * Purpose: counts lines, words and characters, This is a
+ * bare-bones version of the UNIX program wc
+ *
+ * Author: socrates
+ *
+ *********************************************************/
+ package main
+
+ import (
+   "bufio"
+   "fmt"
+   "io"
+   "os"
+ )
+
+ func main() {
+
+   var (
+     nl = 0
+     nw = 0
+     nc = 0
+     inWord = false
+   )
+
+   reader := bufio.NewReader(os.Stdin)
+
+   for {
+      r, _, err := reader.ReadRune()
+       if err != nil {
+         if err != io.EOF {
+           // encountered a real real, exit
+           fmt.Println("error:", err)
+           return 
+         }
+         // reached end of file, exit
+         break
+       }
+       
+     switch r {
+     case ' ':
+       inWord = false
+     case '\t':
+       inWord = false
+     case '\n': 
+        if r == '\n' {
+          nl++
+        }
+        inWord = false
+     default:
+        if inWord == false {
+          inWord = true
+          nw++
+        }
+     }
+     nc++
+   }
+
+   fmt.Printf("nl = %d, nw = %d, nc = %d\n", nl, nw, nc)
+ }
