e01973acd2d619cd008e2be76e58040ae35d584f
socrates <startup.developer84@gmail.com>
2026-03-14 15:25:16 -0400

    added slices.go

---
+++ K_R/ch1/other/slices.go
@@ -0,0 +1,45 @@
+/**********************************************************
+ * File: slices.go
+ *
+ * Purpose: counts the number of occurrences of each digit,
+ * of white space characters (blanks, tabs, newlines) and 
+ * of all other characters.
+ *
+ * Author: socrates
+ *
+ *********************************************************/
+ package main
+
+ import (
+   "bufio"
+   "fmt"
+   "os"
+ )
+
+ func main() {
+
+   reader := bufio.NewReader(os.Stdin)
+   digits := make([]int, 10)
+   nwhite := 0
+   nother := 0
+
+   for {
+     r, _, err := reader.ReadRune()
+     if err != nil {
+       break
+     }
+
+     if r >= '0' && r <= '9' {
+       digits[r - '0']++
+     } else if r == ' ' || r == '\t' || r == '\n' {
+       nwhite++
+     } else {
+       nother++
+     }
+   }
+
+   fmt.Println("digits =", digits)
+   fmt.Println("white spaces =", nwhite)
+   fmt.Println("other characters =", nother)
+ }
+
